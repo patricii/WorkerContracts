77c938fe8395a7425a30ba46a43f954aff98f7cb
PTC <thqv74@gmail.com>
2021-03-24 10:05:54 -0300

    Adicionado o Department no constructor

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,52 +1,61 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.enums.Workerlevel;
 
 public class Worker {
-	
+
 	private String name;
 	private Workerlevel level;
 	private Double baseSalary;
-	
+
 	private Department department;
-	private List<HourContract> contracts;
-	
+	private List<HourContract> contracts = new ArrayList<>();
+
 	public Worker() {
-		
+
 	}
-	public Worker(String name, Workerlevel level, Double baseSalary) {
+
+	public Worker(String name, Workerlevel level, Double baseSalary, Department department) {
 		this.name = name;
 		this.level = level;
 		this.baseSalary = baseSalary;
+		this.department = department;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Workerlevel getLevel() {
 		return level;
 	}
+
 	public void setLevel(Workerlevel level) {
 		this.level = level;
 	}
+
 	public Double getBaseSalary() {
 		return baseSalary;
 	}
+
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
+
 	@Override
 	public String toString() {
 		return "Worker [name: " + name + ", Job Grade: " + level + ", Salary: " + baseSalary + "]";
 	}
-	
+
 	public void AddContract() {
-		
+
 	}
-	
 
 }
